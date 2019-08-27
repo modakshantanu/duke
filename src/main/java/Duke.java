586bb4b685c42e29cdc8a8d63c4c71f2cccab8f9
modakshantanu <modakshantanu@gmail.com>
2019-08-27 10:07:49 +0800

    Level 1 - Echo statements entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,29 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String horizontalLine = "____________________________________________________________";
+
         System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("end")) {
+                System.out.println(horizontalLine);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                return;
+            }
+
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+
+        }
+
+
     }
 }
